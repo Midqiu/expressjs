2f1afaccfbdda39e652f3445608091c3d090e1a0
李中秋 <Midqiu@users.noreply.github.com>
2022-06-29 17:44:10 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,22 @@ app.get("/", async (req, res) => {
   res.json({ Hello: "World" });
 });
 
-app.get("/headers", async (req, res) => {
+app.get("/header", async (req, res) => {
   res.json({ headers:req.headers });
 });
 
+app.get("/body", async (req, res) => {
+  res.json({body:req.body});
+});
+
+app.get("/echo", async (req, res) => {
+  var data={
+    query:req.query,
+    body:req.body,
+    header:req.headers
+  }
+  res.json(data);
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
